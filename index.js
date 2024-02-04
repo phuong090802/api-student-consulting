a2f48bab3fd345ade5842032c3c6c494c6d1f1ef
phuong090802 <20110547@student.hcmute.edu.vn>
2024-02-04 16:20:00 +0700

    Update cors socket

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const io = new Server(server, {
     methods: ['GET', 'POST'],
     credentials: true,
   },
-  transports: ['websocket', 'polling'],
-  allowUpgrades: true,
-  allowEIO3: true,
+  transports: [ "websocket" ],
 });
 
 socket(io);
