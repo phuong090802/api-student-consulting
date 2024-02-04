b3ed3dc1f6c16f305c59cd1ed80a8ef3b9c01037
phuong090802 <20110547@student.hcmute.edu.vn>
2024-02-04 13:40:01 +0700

    Update cors header

--- configs/cors.js
+++ configs/cors.js
@@ -12,6 +12,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 export default corsOptions;
