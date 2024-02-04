41d87d6dff6fe0032390a6608da86ab390fec4d4
phuong090802 <20110547@student.hcmute.edu.vn>
2024-02-04 13:50:28 +0700

    Update cors

--- configs/cors.js
+++ configs/cors.js
@@ -1,18 +1,10 @@
-import ErrorHandler from '../utils/ErrorHandler.js';
 
-const WHITE_LIST = [process.env.FRONTEND_URL];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (WHITE_LIST.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new ErrorHandler(403, 'Not allowed by CORS', 4019));
-    }
-  },
-  credentials: true,
+  origin: [process.env.FRONTEND_URL],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 export default corsOptions;
